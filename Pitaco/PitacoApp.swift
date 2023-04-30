31aad79573bb8ae8d805bc255bb4d60eba735ae7
Matheus Ribeiro de Araújo <83601164+KingOfHell6@users.noreply.github.com>
2023-04-30 18:08:44 -0300

    Initial Commit

---
+++ Pitaco/PitacoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitacoApp.swift
+//  Pitaco
+//
+//  Created by Matheus Araújo on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PitacoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
